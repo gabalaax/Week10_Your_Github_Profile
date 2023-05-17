66fd133512b051cfeb04dd8bc30498c306164b60
gabalaax <rahmaabdikaniwaxbarasho@gmail.com>
2023-05-17 19:12:12 +0300

    waan dhameeyay

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React from "react";
-
-// Import the "useState" and "useEffect" hooks from React
-// Soo jiido "useState" iyo "useEffect"
-
-// Import the "MyProfile" component
-// Soo jiido "MyProfile" component-ka
-
-// Import the "axios" library
-// Soo jiido "axios" hoos
-
+import React, { useState, useEffect } from "react";
+import MyProfile from "./components/MyProfile";
+import axios from "axios";
 import "./App.css";
-
 function App() {
-
-  // Create 3 states, "profile", "followers", and "following"
-  // Samee 3 state, "profile", "followers", iyo "following"
-
-
-  // API For Profile = https://api.github.com/users/<your-github-username>
-  // API for Followers = https://api.github.com/users/<your-github-username>/followers
-  // API for Following = https://api.github.com/users/<your-github-username>/following
-
-  // Use axios to fetch data from the API using the useEffect hook
-  // Halkaan isticmaal axios adigoo kasoo jiidanaayo waxaa u baahantahay API, useEffect hook-na isticmaal
-    
-  return (
-    <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
+    const [profile, setProfile ] = useState([]);
+    const [followers, setFollowers ] = useState([]);
+    const [following, setFollowing ] = useState([]);
+    useEffect(() => {
+      axios.get("https://api.github.com/users/gabalaax")
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }, []);
+    useEffect(() => {
+      axios.get("https://api.github.com/users/gabalaax/followers")
+        .then((res) => {
+          setFollowers(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }, []);
+  
+    useEffect(() => {
+      axios.get("https://api.github.com/users/gabalaax/following")
+        .then((res) => {
+          setFollowing(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }, []); 
+    return (
+      <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
       <div className="h-[140px] bg-gradient-to-r from-cyan-500 to-blue-500"></div>
-
-      {/* Show "MyProfile" component here and give it 3 props, "profile", "followers", and "following" */}
-      {/* Halkaan soo gali "MyProfile", 3 props-na sii, "profile", "followers", iyo "following" */}
-
+      <MyProfile profile ={ profile } followers ={ followers } following ={ following } />
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 // import "BrowserRouter" from "react-router-dom"
 
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +14,8 @@ root.render(
     {/* Wrap the "App" component with "BrowserRouter" */}
     {/* "BrowserRouter" hugi "App" */}
     
-    <App />
+   <BrowserRouter>
+      <App />
+   </BrowserRouter>
   </React.StrictMode>
 );
